3c13f12706480c3bf806f84cb76d08a8f50a32a2
ffmac <ffmac@ffmacdeMacBook-Air.local>
2020-07-24 15:46:56 +0800

    添加

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id='app'>
+   <h1>aaa</h1>
+   <!-- 根组件添加路由映射 -->
+   <router-view></router-view>
+ </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+
+</script>
+
+<style>
+/* 引入字体图标 */
+@import url('http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css')
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 引入全局初始化样式
+import '@/styles/reset.less'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
